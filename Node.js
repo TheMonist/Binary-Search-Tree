d42d0ec5c7cfc004ecbfd5aabbd67980a738eb0e
TheMonist <rogerrosena@gmail.com>
2023-08-06 11:10:43 -0400

    Creating Node factory function

---
+++ Node.js
@@ -0,0 +1,10 @@
+//Node Factory
+const Node = (data) => {
+    return {
+    data: data,
+    rightChild: null,
+    leftChild: null,
+    }
+}
+
+export default Node;
---
+++ Tree.js
@@ -0,0 +1,20 @@
+import Node from "./Node";
+
+//Tree Factory
+const Tree = (inputArray) => {
+    const buildTree = (arr, start, end) => {
+        
+        //Base case
+        if (start > end) return null;
+
+        //Find the middle
+        let mid = parseInt(start + end) / 2;
+        let root = Node(arr[mid]);
+
+        //Recursively bulid left and right tree roots
+        root.leftChild = buildTree(arr, start, mid - 1);
+        root.rightChild = buildTree(arr, mid - 1, end);
+
+        return root
+    }
+}
